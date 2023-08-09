60a62cf865e496a2b2454f26b28f8c3d96f0c7eb
Ravi Kumar <muruganravi8301@gmail.com>
2023-08-09 17:02:21 +0530

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,19 @@
+import 'zone.js';
+import 'hammerjs';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { RecaptchaComponent } from 'ng-recaptcha';
+RecaptchaComponent.prototype.ngOnDestroy = function() {
+  if (this.subscription) {
+    this.subscription.unsubscribe();
+  }
+}
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
